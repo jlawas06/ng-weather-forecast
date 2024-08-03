7bfb16f425058c9057664d4acda53607c25dac29
jlawas06 <jlawas06@gmail.com>
2024-08-04 00:33:27 +0800

    feat: add landing page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './features/landing';
 import { LayoutContainerComponent } from './layout/containers/layout-container/layout-container.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LayoutContainerComponent,
-    children: [],
+    children: [
+      {
+        path: '',
+        component: LandingComponent,
+      },
+    ],
   },
 ];
