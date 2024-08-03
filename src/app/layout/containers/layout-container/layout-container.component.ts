2fd5f42800bbd7d11e4c90656c17581651195f63
jlawas06 <jlawas06@gmail.com>
2024-08-03 22:52:08 +0800

    feat: added authentication handling

--- src/app/layout/containers/layout-container/layout-container.component.ts
+++ src/app/layout/containers/layout-container/layout-container.component.ts
@@ -1,11 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-layout-container',
   templateUrl: './layout-container.component.html',
   styleUrl: './layout-container.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LayoutContainerComponent {
+  auth = inject(AuthService);
 
+  isAuthenticating$ = this.auth.isAuthenticated$;
+
+  onLogout() {
+    this.auth.logout({
+      logoutParams: { returnTo: window.location.origin },
+    });
+  }
 }
