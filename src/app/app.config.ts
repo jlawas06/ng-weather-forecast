ab618f9e9b547f8e6553e1130cf984eae1835a28
jlawas06 <jlawas06@gmail.com>
2024-08-03 20:54:58 +0800

    feat: add and configure auth0

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { AuthModule } from '@auth0/auth0-angular';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(
+      AuthModule.forRoot({
+        domain: '{AUTH0_DOMAIN}',
+        clientId: '{AUTH0_CLIENT_ID}',
+        authorizationParams: {
+          redirect_uri: window.location.origin,
+        },
+      })
+    ),
+  ],
 };
